9394820de0e8dde60d5e9257f4469f7308dda92e
Kirknasty54 <Kirkpatrick545454@gmail.com>
2023-12-12 00:48:11 -0600

    continued basic setup

--- main.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-def main():
-    print('helllo world')
-main()
---
+++ scripts/main.py
@@ -0,0 +1,10 @@
+import sqlite3
+import screens
+import bcrypt
+def main():
+    screens.loginScreen()
+    #conn = sqlite3.connect('user_accounts.db')
+    #conn2 = sqlite3.connect('book_list.db')
+    #cursor = conn2.cursor()
+    #cursor.execute('create table if not exists(')
+main()
